fbbacfda85d78f610bf590c714cd2d9117f80f47
joanitac <joanita@statuscreative.co.uk>
2016-10-08 03:04:15 +0100

    Initial Commit

---
+++ getDemoTH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  getDemoTH
+//
+//  Created by Joanita Cordova on 08/10/2016.
+//  Copyright © 2016 Joanita Cordova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
